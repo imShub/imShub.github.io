0a7e4531a815a969e8adbf367cc5ebed182f9225
Shubham M. Waghmare <waghmareshubham132@gmail.com>
2023-12-30 22:51:31 +0530

    Portfolio Updated with Current Details

--- forms/contact.php
+++ forms/contact.php
@@ -1,15 +1,14 @@
 <?php
 
-  $receiving_email_address = 'waghmareshubham132@gmail.com';
+$receiving_email_address = 'waghmareshubham132@gmail.com';
 
-  $from_name = $_POST['name'];
-  $from_email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
+$from_name = $_POST['name'];
+$from_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-  $mailheader = "From: " . $from_name . "\r\n";
+$mailheader = "From: " . $from_name . "\r\n";
 
-  mail($receiving_email_address,$subject,$message,$mailheader) or die("Error!");
+mail($receiving_email_address, $subject, $message, $mailheader) or die("Error!");
 
-  header('Location: index.html');
-?>
+header('Location: index.html');
